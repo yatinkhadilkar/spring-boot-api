c69f0f6325ee9450e9e57a3ea75147ee3f335637
ykhadilkar <ykhadilkar@pivotal.io>
2020-03-01 16:12:27 -0500

    New boot project

---
+++ src/test/java/net/khadilkar/springbootapi/SpringBootApiApplicationTests.java
@@ -0,0 +1,13 @@
+package net.khadilkar.springbootapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
