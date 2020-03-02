f336927257a8b4dac73742bd0a161acae93fdc0f
ykhadilkar <ykhadilkar@pivotal.io>
2020-03-01 21:57:32 -0500

    correcting JAR path

--- src/main/java/net/khadilkar/springbootapi/SpringBootApiApplication.java
+++ src/main/java/net/khadilkar/springbootapi/SpringBootApiApplication.java
@@ -2,12 +2,21 @@ package net.khadilkar.springbootapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpringBootApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootApiApplication.class, args);
+	}
+
+	@GetMapping("/")
+	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return String.format("Hello %s!", name);
 	}
 
 }
