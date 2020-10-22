acf1da3a1d99520c6dd736c4bbf8db35c31f0259
ykhadilkar <ykhadilkar@pivotal.io>
2020-10-22 14:14:42 -0400

    removing wrong tests

--- src/test/java/net/khadilkar/springbootapi/SpringBootApiApplicationTests.java
+++ src/test/java/net/khadilkar/springbootapi/SpringBootApiApplicationTests.java
@@ -1,10 +1,7 @@
 package net.khadilkar.springbootapi;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.boot.web.server.LocalServerPort;
 
 @SpringBootTest
 class SpringBootApiApplicationTests {
@@ -12,17 +9,4 @@ class SpringBootApiApplicationTests {
 	@Test
 	void contextLoads() {
 	}
-
-	@LocalServerPort
-	private int port;
-		
-	@Autowired
-	private TestRestTemplate restTemplate;
-		
-	@Test
-	public void testEndpoint() {
-		assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/",
-		String.class)).contains("Hello");
-	}
-
 }
