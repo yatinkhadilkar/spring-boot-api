00a1426574b9a920b8f87d82d164c3ab6af3956c
ykhadilkar <ykhadilkar@pivotal.io>
2020-10-23 11:37:53 -0400

    K8s Secret / Config Integration

--- src/main/java/net/khadilkar/springbootapi/SpringBootApiApplication.java
+++ src/main/java/net/khadilkar/springbootapi/SpringBootApiApplication.java
@@ -1,5 +1,6 @@
 package net.khadilkar.springbootapi;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,14 +9,20 @@ import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
+
 public class SpringBootApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootApiApplication.class, args);
 	}
 
+	@Value( "${DB_USERNAME}" )
+	String DB_USERNAME;
+
 	@GetMapping("/")
 	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+
+		System.out.println("Environment Variables ..."+ DB_USERNAME);
 		return String.format("Hello %s!", name);
 	}
 
